9ba43a4d2bdd2ed39f48c5b346b4b74117bd92d5
Bhavisha Panchal <145912697+Bhavisha-16@users.noreply.github.com>
2025-03-02 21:00:53 +0530

    Update extractive_abstractive_summarization.py

--- extractive_abstractive_summarization.py
+++ extractive_abstractive_summarization.py
@@ -18,6 +18,7 @@ Despite these challenges, technology continues to drive innovation and progress.
 """
 
 
+import sumy
 import streamlit as st
 import random
 from sumy.parsers.plaintext import PlaintextParser
